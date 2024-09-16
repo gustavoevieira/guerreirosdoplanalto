54e96645693f3fd51015c4d3c21c37044cf65a8a
gustavoevieira <gustta.eudes@gmail.com>
2024-09-16 12:13:31 -0300

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+let currentSlide = 0;
+const slides = document.querySelectorAll('.carousel img');
+
+// Mostrar a imagem inicial
+slides[currentSlide].classList.add('active');
+
+// Função para ir para o próximo slide
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+// Função para ir para o slide anterior
+function prevSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+// Troca automática de slides a cada 4 segundos
+setInterval(nextSlide, 3200);
